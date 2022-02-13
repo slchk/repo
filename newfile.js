c53ee2e3881d1ef8c0dafd725ad657b92b9cba1c
Elena Sluchak <lena.sluchak@gmail.com>
2022-02-13 19:27:28 +0300

    adding hometask4

---
+++ newfile.js
@@ -0,0 +1,22 @@
+
+console.log('foo'+ false);
+console.log('asd' + 5);
+console.log(5 + true);
+
+
+
+console.log('foo' * false);
+console.log('asd' * 5);
+console.log(5 * false);
+
+
+
+console.log('hkm'/ true);
+console.log('qasd' / 4);
+console.log(10 / false);
+
+
+
+console.log(Number(false));
+console.log(String(123));
+console.log(Boolean([]));
