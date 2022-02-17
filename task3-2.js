1b2c8d6c6bdcd301eceb33b90c7a91c808dcbfb9
Elena Sluchak <lena.sluchak@gmail.com>
2022-02-17 18:37:16 +0300

    hometask5

---
+++ task3-2.js
@@ -0,0 +1,23 @@
+// let numObj = 12345.6789;
+// numObj.toFixed(2);
+// console.log(numObj.toFixed(2));
+
+let getNumber;
+getNumber.toFixed(2);
+function getNumber(min, max, length, sum) {
+    return Array.from(
+        { length },
+        (_, i) => {
+            let smin = (length - i - 1) * min,
+                smax = (length - i - 1) * max,
+                offset = Math.max(sum - smax, min),
+                random = Math.min(sum - offset, max - offset, sum - smin - min),
+                value = Math.floor(Math.random() * random + offset);
+
+            sum -= value; // sum = sum - value
+            return value;
+        }
+    );
+}
+
+console.log(Array.from({ length: 10 }, _ => getNumber.toFixed(2)(0, 10, 3, 10).join(' ')));
